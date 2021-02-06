8ef9b8e7940b42802aec8c8b6450525cf8e1c24f
MyOwnSling <myownsling@gmail.com>
2021-02-06 16:07:13 -0700

    Added alert cooldown

--- src/notifiers/pbnotify.py
+++ src/notifiers/pbnotify.py
@@ -1,7 +1,35 @@
 import os
 import json
+import time
 from pushbullet import Pushbullet as pb
-# from . import Notifier
+
+
+def cooldown(notify_func):
+    """
+    Check to see if the given notification title has been sent within
+     the configured cooldown window
+    """
+
+    def _cooldown_wrapper(self, *args, **kwargs):
+
+        # We know the title is always going to be the first arg
+        title = args[0]
+
+        # Check to see if this notification has been sent at all
+        if not title in self.alert_records:
+            self.alert_records[title] = time.time()
+            return notify_func(self, *args, **kwargs)
+        
+        # Get the time since last notification and check it against the cooldown window
+        span = time.time() - self.alert_records[title]
+        if span > self.cooldown:
+            self.alert_records[title] = time.time()
+            return notify_func(self, *args, **kwargs)
+
+        # Cooldown hasn't expired; do nothing
+        return
+
+    return _cooldown_wrapper
 
 class PBNotifier():
     def __init__(self, registrants):
@@ -9,6 +37,11 @@ class PBNotifier():
             raise ValueError("Missing PushBullet auth token")
         self.pb_client = pb(os.environ["PB_AUTH"])
         self.pb_chats = dict()
+
+        # Cooldown values used to keep repeated alerts
+        self.cooldown = 120 * 60 # Cooldown in seconds
+        self.alert_records = dict()  # Track alert titles and the last alert time
+
         # Expect a dictionary with 'pb' key whose value is an array of registrant emails
         try:
             self.registrants = json.loads(registrants)['pb']
@@ -26,7 +59,9 @@ class PBNotifier():
             if registrant not in self.pb_chats:
                 chat = self.pb_client.new_chat(registrant, registrant)
                 self.pb_chats[chat.email] = chat
-    
+
+
+    @cooldown
     def notify(self, title, msg, recipients=None):
         """Notify the given reicpients (recipient is a destination email)"""
 
